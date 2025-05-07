f8065359dfc5445740dddd263fa8339986f32be7
Gao Saipin <gao_gmm@live.com>
2025-05-06 20:54:13 -0700

    finished exercise-three.js

---
+++ lesson04/exercise-three.js
@@ -0,0 +1,10 @@
+let fruits = ['apple', 'banana', 'coconut', 'grape', 'orange']
+for (let i = 1; i < fruits.length; i++) {
+    console.log(i + 1 + ',' + fruits[i]);
+}
+
+// 1,apple
+// 2,banana
+// 3,grape
+// 4,grape
+// 5,orange
