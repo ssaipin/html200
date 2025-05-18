e59a01dc642f49e9b1a453d36891956f8728388b
Gao Saipin <gao_gmm@live.com>
2025-05-18 00:21:27 -0700

    created no joker.js

---
+++ lesson04/no-joker.js
@@ -0,0 +1,24 @@
+// Create an array with two arrays inside of it 
+// The first is the four suits of cards
+// The second is the name of each card ”ace”, “one”…”king”
+// Console log them in order
+
+let suits = ['diamonds', 'hearts', 'clubs', 'spades'];
+let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  for (let value of values) {
+    console.log(`${value} of ${'K'}`);
+  }
+  // A of K
+  // 2 of K
+  // 3 of K
+  // 4 of K
+  // 5 of K
+  // 6 of K
+  // 7 of K
+  // 8 of K
+  // 9 of K
+  // 10 of K
+  // J of K
+  // Q of K
+  // K of K
